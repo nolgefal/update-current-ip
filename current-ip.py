6416c684dfe8244bca27da170fa37c8dc50e55c2
Lh Long <lhlong09t4@gmail.com>
2018-07-27 16:59:41 +0700

    update method

--- current-ip.py
+++ current-ip.py
@@ -4,44 +4,39 @@ import requests
 
 from oauth2client.service_account import ServiceAccountCredentials
 
-# update ip to wordsheet
-def update_cel(wordsheet):
-    res = requests.get('https://ipinfo.io/ip')
-    wordsheet.update_acell('A2', str(res.content))
-    
 # check internet
 def internet_on():
     try:
-        requests.get('http://216.58.192.142', timeout=1)
+        global ip
+        ip = requests.get('https://ipinfo.io/ip', timeout=1)
         return True
     except: 
         return False
+    
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('current-ip.json', scope)
 
-url = 'https://docs.google.com/spreadsheets/d/1k9pT0QNiz_wuILLgG9O7p_40IWSRjN8nn62f0XEvPEk/edit?usp=sharing'
-print(url) # google sheet
-
-if (internet_on()):
-    try:
-        gc = gspread.authorize(credentials)
-        # sh = gc.create('current-ip') # create sheet-name current-ip
-
-        # open link of wordsheet
-        
-        wks = gc.open_by_url(url).sheet1
-        wks.update_acell('A1', 'IP')
-        print("Updated at: %s" %time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time())))
-    except:
-        print("Can't connect wordsheet now.")
-
 while True:
-    if (internet_on()):
+    if (internet_on()):        
         try:
-            update_cel(wks)
+            # credentials
+            gc = gspread.authorize(credentials)
+            sh = gc.open('current-ip')
+        except gspread.SpreadsheetNotFound:
+            # print('Sheet is not found')
+            sh = gc.create('current-ip')
+            sh.share('your_email_address', perm_type='user', role='writer')
+            sh.share('your_friend_email_address', perm_type='user', role='writer')
+            sh.sheet1.update_acell('A1', 'IP')
+            sh.sheet1.update_acell('A2', str(ip.content))
+        except:
+            print("Can't authorize")
+        
+        try:            
+            sh.sheet1.update_acell('A2', str(ip.content))
         except:
             print("Can't update cel now.")
     else:
